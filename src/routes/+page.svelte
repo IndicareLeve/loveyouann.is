e14d6d320516ff8380e48e6d8544dc5d839eb120
IndicareLeve <indicare.leve@gmail.com>
2022-10-26 15:57:15 +0200

    migrate to sveltekit and small update

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<main class="grid">
+	<span class="row1 text-left">
+		Super
+	</span>
+	
+	<span class="row2 text-right">
+		FORTE
+	</span>
+	<div class="row3 fill">
+		<img alt="flying-cat" src="https://media2.giphy.com/media/5IAoDGpfh0c1y/giphy.gif?cid=790b7611ea75c6f3a3a17aa1d739cdf292866e00ebd9b68a&rid=giphy.gif&ct=g" />
+		<!-- <iframe title="flying-cat" src="https://giphy.com/embed/5IAoDGpfh0c1y" width="100%" height="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe> -->
+	</div>
+	<span class="row4 text-center text-big">
+		Annys!
+	</span>
+</main>
+
+<script lang="ts">
+
+</script>
