b767446cdd663360acb6f3d24d0db1e250de54f1
Indicareleve <indicare.leve@gmail.com>
2023-02-13 13:18:38 +0100

    happy bday +23

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
-<div class="message">
+<div>
 	<div class="grid">
-		<span class="row1 text-left">Buon</span>
-		<span class="row2 text-right">Natale</span>
-		<span class="row3 text-center">Sweetie ♡</span>
+		<span class="row1">Buon</span>
+		<span class="row2">Compleannooooooooo</span>
+		<div class="row3">
+			<img class="gif" src="pallas.gif" alt="pallas cat" />
+			<span class="caption">Super forte e tenera <br /> come il gatto pallas!</span>
+			<span class="caption1">Proud of U 🥰</span>
+			<span class="caption2">Ti voglio bene ❣️</span>
+		</div>
+		<span class="row4">Annysssssssssssssss</span>
 	</div>
-</div>
-<div class="snow">
-	<div />
 </div>
